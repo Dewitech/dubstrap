8db62dad0281021333076c6fe289bf34ca6da380
ryanhidajat <insomni4k@yahoo.com>
2013-08-20 16:52:18 +0700

    remove customcodes.js

--- functions.php
+++ functions.php
@@ -227,24 +227,7 @@ function dubstrap_widgets_init() {
 
 
 }
-add_action( 'init', 'dubstrap_widgets_init' );
-add_action('init', 'add_button');  
-function add_button() {  
-   if ( current_user_can('edit_posts') &&  current_user_can('edit_pages') )  
-   {  
-     add_filter('mce_external_plugins', 'add_plugin');  
-     add_filter('mce_buttons', 'register_button');  
-   }  
-}  
-function register_button($buttons) {  
-   array_push($buttons, "quote");  
-   return $buttons;  
-}  
 
-function add_plugin($plugin_array) {  
-   $plugin_array['quote'] = get_bloginfo('template_url').'/includes/customcodes.js';  
-   return $plugin_array;  
-}  
 
 // Numeric Page Navi
 function page_navi($before = '', $after = '') {
