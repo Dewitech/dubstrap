4426b9da32494bd81b67a2b78cd572acad1998fa
ryanhidajat <insomni4k@yahoo.com>
2013-08-20 20:28:20 +0700

    author.php

--- author.php
+++ author.php
@@ -7,27 +7,29 @@ get_header(); ?>
 
 	<article class="container">
 		<div class="row">
-		<div class="span8">
-			<div class="page-header"><h1 class="archive_title h2">
-						<span><?php _e("Posts By:", "bonestheme"); ?></span> 
-						<?php 
-							// If google profile field is filled out on author profile, link the author's page to their google+ profile page
-							$curauth = (get_query_var('author_name')) ? get_user_by('slug', get_query_var('author_name')) : get_userdata(get_query_var('author'));
-							$google_profile = get_the_author_meta( 'google_profile', $curauth->ID );
-							if ( $google_profile ) {
-								echo '<a href="' . esc_url( $google_profile ) . '" rel="me">' . $curauth->display_name . '</a>'; 
-						?>
-						<?php 
-							} else {
-								echo get_the_author_meta('display_name', $curauth->ID);
-							}
-						?>
-					</h1>
+			<div class="span12">
+				<div class="page-header"><h1 class="archive_title h2">
+							<span><?php _e("Posts By:", "bonestheme"); ?></span> 
+							<?php 
+								// If google profile field is filled out on author profile, link the author's page to their google+ profile page
+								$curauth = (get_query_var('author_name')) ? get_user_by('slug', get_query_var('author_name')) : get_userdata(get_query_var('author'));
+								$google_profile = get_the_author_meta( 'google_profile', $curauth->ID );
+								if ( $google_profile ) {
+									echo '<a href="' . esc_url( $google_profile ) . '" rel="me">' . $curauth->display_name . '</a>'; 
+							?>
+							<?php 
+								} else {
+									echo get_the_author_meta('display_name', $curauth->ID);
+								}
+							?>
+						</h1>
+				</div>
 			</div>
 		
 			<?php if ( have_posts() ) : ?>
 				<?php /* The loop */ ?>
 				<?php while ( have_posts() ) : the_post(); ?>
+					<div class="span4 item-cat">
 							<header>
 								
 								<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
@@ -37,25 +39,25 @@ get_header(); ?>
 							</header> <!-- end article header -->
 						
 							<section class="post_content">
-							
-								<?php the_post_thumbnail( 'dubstrap-thumb-300' ); ?>
-							
-								<?php the_excerpt(); ?>
-						
+							<?php if ( has_post_thumbnail() ) : 
+							the_post_thumbnail( 'dubstrap-featured' );
+							else :
+								the_excerpt();
+							endif; 
+							?>
 							</section> <!-- end article section -->
 							
 							<footer>
-								
+						
 							</footer> <!-- end article footer -->
+					</div>
 				<?php endwhile; ?>
 
-				<?php page_navi(); ?>
+				
 
 			<?php else : ?>
 				<?php get_template_part( '404', 'none' ); ?>
 			<?php endif; ?>
-		</div>
-		<?php get_sidebar(); ?>
 		</div><!-- /row -->
-	</article><!-- #primary -->
+	</article><!-- article -->
 <?php get_footer(); ?>
