14980c3da2b5e37e8d1f58ed3a14ffede7a656b9
ryanhidajat <insomni4k@yahoo.com>
2013-08-21 15:27:52 +0700

    archive update

--- archive-1columns.php
+++ archive-1columns.php
@@ -7,8 +7,11 @@
 								
 								<h3 class="h2"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
 								
-								<p class="meta"><?php _e("Posted", "dubstrap"); ?> <time datetime="<?php echo the_time('Y-m-j'); ?>"><?php the_date(); ?></time> <?php _e("by", "dubstrap"); ?> <?php the_author_posts_link(); ?> <span class="amp">&</span> <?php _e("filed under", "dubstrap"); ?> <?php the_category(', '); ?>.</p>
-							
+								<ul class="meta">
+									<li><i class="icon-user"></i> <?php the_author_posts_link(); ?></li>
+									<li><i class="icon-calendar-empty"></i> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F j, Y'); ?></time></li>
+									<li><i class="icon-folder-open-alt"></i><?php the_category(', '); ?>.</li>
+								</ul>
 							</header> <!-- end article header -->
 						
 							<section class="post_content">
