b3a2a73fde9adad17f0f67e7b96f4d1304210125
ryanhidajat <insomni4k@yahoo.com>
2013-08-21 16:27:39 +0700

    jquery tweet default update

--- footer.php
+++ footer.php
@@ -84,10 +84,10 @@
 				  avatar_size: 48,
 				  count: 1,
 				  auto_join_text_default: " we said, ",
-				  auto_join_text_ed: " we ",
-				  auto_join_text_ing: " we were ",
-				  auto_join_text_reply: " we replied ",
-				  auto_join_text_url: " we were checking out ",
+		//		  auto_join_text_ed: " we ",
+		//		  auto_join_text_ing: " we were ",
+		//		  auto_join_text_reply: " we replied ",
+		//		  auto_join_text_url: " we were checking out ",
 				  loading_text: "loading tweets..."
 				});
 			  });	
