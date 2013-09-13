3910ae7db8cd354ed1c11f66d5b1ad8965a8a85f
ryanhidajat <insomni4k@yahoo.com>
2013-09-14 05:34:09 +0700

    searchform.php

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form role="search" method="get" id="searchform" action="<?php echo $homeurl ?>">
+	<div>
+		<input type="text" value="" name="s" id="s" class="search" placeholder="Search..." />
+	</div>
+</form>
