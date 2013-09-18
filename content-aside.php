41add40c4eeec7e1f151d59540407725056fc353
ryanhidajat <insomni4k@yahoo.com>
2013-09-18 07:36:39 +0700

    single post typhography hierarchy

--- content-aside.php
+++ content-aside.php
@@ -4,7 +4,7 @@
  */
 ?>
 		<header class="page-title">
-			<h3><?php the_title();?></h3>
+			<h2><?php the_title();?></h2>
 			<ul class="meta">
 				<li><i class="icon-user"></i> <?php the_author_posts_link(); ?></li>
 				<li><i class="icon-calendar-empty"></i> <time datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F j, Y'); ?></time></li>
